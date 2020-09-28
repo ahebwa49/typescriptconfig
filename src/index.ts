55f217bfd2c71cab349e47d56ae1d4f9f815a766
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-09-28 17:51:14 +0300

    add css to the typescript config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-window.addEventListener("load", () => {
-  const header = document.createElement("h1");
-  header.innerText = "Webpack❤️TS";
+import * as _ from "lodash";
+import "./styles.css";
 
-  const body = document.querySelector("body");
-  body.appendChild(header);
-});
+window.addEventListener("load", () => {
+    const header = document.createElement("h1");
+    header.innerText = "Webpack❤️TS"
+
+    const body = document.querySelector("body");
+    body.appendChild(header);
+
+    const food = [{id: 0, name: "pizza"}, {id: 1, name: "sushi"}]
+
+    const foodElement = document.createElement("h2")
+    foodElement.innerText = _.find(food, {id: 0})?.name;
+
+    body.appendChild(foodElement);
+})
